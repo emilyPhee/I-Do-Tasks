3bc53fc5a4685974a00f2cae3082bb6a6e3ea0ff
Emily Phee <ephee@uci.edu>
2020-09-23 22:48:46 -0700

    Add new file app.js and modify the unit px to rem on style.css

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+const taskInputField = document.querySelector('.input');
+const button = document.querySelector('button');
+
+
+console.log(taskInputField); 
+
+// taskInputField.addEventListener()
+button.addEventListener('click', handleAddTask);
+
+function handleAddTask(e) {
+  //
+}
