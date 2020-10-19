0b22963b43fcb67f57b6b64a38d5e736c5604203
Emily Phee <ephee@uci.edu>
2020-10-19 16:11:08 -0700

    Modify the input field position to the top and add design of check box

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 'use strict';
-const taskInputField = document.querySelector('.input');
-const button = document.querySelector('button');
+const ulElement = document.querySelector('ul');
+const input = document.querySelector('input');
+const addButton = document.querySelector('button');
 
 
-console.log(taskInputField); 
 
-// taskInputField.addEventListener()
-button.addEventListener('click', handleAddTask);
+// handle the click the ADD button
+addButton.addEventListener('click', clickedButton);
 
-function handleAddTask(e) {
-  //
+function clickedButton(e) {
+  // storing the current value of the input element in variable
+  const currentValue = input.value;
+  // remove it after clicked button
+  input.value = '';
+
+  // list element
+  const listItem = document.createElement('li');
+  listItem.className = "task-item";
+  
+
+  // const checkBox = document.createElement('input');
+  // checkBox.type = "checkbox";
+  // checkBox.className = "list-check-box";
+  
+  // delete button element
+  const removeIcon = document.createElement('button');
+  removeIcon.className = "btn";
+  const trashIcon = document.createElement('i');
+  trashIcon.className = "fas fa-trash trash-icon";
+  removeIcon.appendChild(trashIcon);
+
+  // list input element
+  const spanElement = document.createElement('span');
+
+  listItem.appendChild(removeIcon);
+  // listItem.appendChild(checkBox);
+  listItem.appendChild(spanElement);
+
+  spanElement.textContent = currentValue;
+
+  ulElement.appendChild(listItem);
+
+
+
+
+  
+
+
+
+
 }
