05b5bcf867816eec018fdaa1d4cca78f1348caea
Emily Phee <ephee@uci.edu>
2020-12-19 16:44:54 -0800

    modify add function and add delete task function in app.js

--- app.js
+++ app.js
@@ -1,52 +1,42 @@
-'use strict';
-const ulElement = document.querySelector('ul');
-const input = document.querySelector('input');
-const addButton = document.querySelector('button');
+
+const ulElement = document.querySelector('.unorder-list');
+const taskForm = document.querySelector('#task-form');
+const taskInput = document.querySelector('.task-input');
+
+// Load all event listners
+loadEventListeners();
+
+function loadEventListeners() {
+  taskForm.addEventListener('submit', addTaskToList);
+  document.body.addEventListener('click', handleDeleteTask);
+}
+
+function addTaskToList(e) {
+  if (taskInput.value === '') {
+    alert('Please add a task :)');
+  } else {
+    const li = document.createElement('li');
+    li.className = 'task-item';
+    li.innerHTML = listElementGenerator(taskInput.value);
+    ulElement.appendChild(li);
+    console.log(li);
+    taskInput.value = '';
+  }
+  e.preventDefault();
+}
+
+function listElementGenerator(task) {
+  const liElementChild =`
+    <input type="checkbox" class="list-check-box">
+    <span>${task}</span><button class="btn"><i class="fas fa-trash trash-icon"></i></button>
+  `;
+  return liElementChild;
+}
 
 
-
-// handle the click the ADD button
-addButton.addEventListener('click', clickedButton);
-
-function clickedButton(e) {
-  // storing the current value of the input element in variable
-  const currentValue = input.value;
-  // remove it after clicked button
-  input.value = '';
-
-  // list element
-  const listItem = document.createElement('li');
-  listItem.className = "task-item";
-  
-
-  // const checkBox = document.createElement('input');
-  // checkBox.type = "checkbox";
-  // checkBox.className = "list-check-box";
-  
-  // delete button element
-  const removeIcon = document.createElement('button');
-  removeIcon.className = "btn";
-  const trashIcon = document.createElement('i');
-  trashIcon.className = "fas fa-trash trash-icon";
-  removeIcon.appendChild(trashIcon);
-
-  // list input element
-  const spanElement = document.createElement('span');
-
-  listItem.appendChild(removeIcon);
-  // listItem.appendChild(checkBox);
-  listItem.appendChild(spanElement);
-
-  spanElement.textContent = currentValue;
-
-  ulElement.appendChild(listItem);
-
-
-
-
-  
-
-
-
-
+function handleDeleteTask(e) {
+  if (e.target.parentElement.parentElement.className === 'task-item') {
+    e.target.parentElement.parentElement.remove();
+  }
+  console.log(e.target.parentElement.parentElement.className);
 }
